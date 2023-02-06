f0c2303c0a3a3b26f7875f76bd4c2463a274308c
night-crawler <lilo.panic@gmail.com>
2023-02-06 15:25:25 +0000

    wip

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,31 +1,23 @@
-use std::env;
+use std::collections::{BTreeMap, HashMap};
 use std::fmt::Debug;
 use std::sync::Arc;
-use std::time::Duration;
 
 use anyhow::Result;
 use async_trait::async_trait;
-use futures::{Stream, StreamExt, TryStreamExt};
+use futures::{StreamExt, TryStreamExt};
 use k8s_openapi::api::core::v1::{Namespace, Pod};
 use k8s_openapi::apimachinery::pkg::apis::meta::v1::ObjectMeta;
-use k8s_openapi::Metadata;
+use k8s_openapi::{Metadata, Resource};
 use kanal::AsyncSender;
 use kube::api::{DynamicObject, GroupVersionKind, ListParams};
 use kube::discovery::{verbs, Scope};
 use kube::runtime::reflector::Store;
 use kube::runtime::{reflector, watcher, WatchStreamExt};
-use kube::{discovery, Api, Client, Discovery, Resource, ResourceExt};
-
-pub struct ResourceState<T>
-where
-    T: Metadata<Ty = ObjectMeta> + 'static + Clone + Send + Sync,
-{
-    reader: Store<T>,
-}
+use kube::{Api, Client, Discovery, ResourceExt};
 
 #[async_trait]
 trait SpecViewAdapter {
-    fn kind(&self) -> &'static str;
+    fn gvk(&self) -> GroupVersionKind;
     fn items(&self) -> Vec<ResourceView>;
 }
 
@@ -41,9 +33,8 @@ where
         + for<'de> k8s_openapi::serde::Deserialize<'de>,
     ResourceView: From<Arc<T>>,
 {
-    fn kind(&self) -> &'static str {
-
-        T::KIND
+    fn gvk(&self) -> GroupVersionKind {
+        GroupVersionKind::gvk(T::GROUP, T::VERSION, T::KIND)
     }
 
     fn items(&self) -> Vec<ResourceView> {
@@ -54,29 +45,36 @@ where
     }
 }
 
-#[async_trait]
-impl<T> SpecViewAdapter for ResourceState<T>
-where
-    T: Metadata<Ty = ObjectMeta> + 'static + Clone + Send + Sync,
-    ResourceView: From<Arc<T>>,
-{
-    fn kind(&self) -> &'static str {
-        T::KIND
-    }
-
-    fn items(&self) -> Vec<ResourceView> {
-        self.reader
-            .state()
-            .iter()
-            .map(|resource| Arc::clone(resource).into())
-            .collect()
-    }
-}
-
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum ResourceView {
     PodView(Arc<Pod>),
     NamespaceView(Arc<Namespace>),
+}
+
+impl ResourceView {
+    pub fn gvk(&self) -> GroupVersionKind {
+        let (group, version, kind) = match self {
+            ResourceView::PodView(_) => (Pod::GROUP, Pod::VERSION, Pod::KIND),
+            ResourceView::NamespaceView(_) => {
+                (Namespace::GROUP, Namespace::VERSION, Namespace::KIND)
+            }
+        };
+        GroupVersionKind::gvk(group, version, kind)
+    }
+
+    pub fn name(&self) -> String {
+        match self {
+            ResourceView::PodView(r) => r.name_any(),
+            ResourceView::NamespaceView(r) => r.name_any(),
+        }
+    }
+
+    pub fn namespace(&self) -> String {
+        match self {
+            ResourceView::PodView(r) => r.namespace().unwrap_or_default(),
+            ResourceView::NamespaceView(r) => r.namespace().unwrap_or_default(),
+        }
+    }
 }
 
 impl From<Arc<Pod>> for ResourceView {
@@ -91,105 +89,72 @@ impl From<Arc<Namespace>> for ResourceView {
     }
 }
 
-impl<T> ResourceState<T>
-where
-    T: Metadata<Ty = ObjectMeta>
-        // + Resource
-        + 'static
-        + Clone
-        + Debug
-        + Send
-        + Sync
-        + for<'de> k8s_openapi::serde::Deserialize<'de>,
-    ResourceView: From<Arc<T>>, // <T as Resource>::DynamicType: Default + Eq + Hash + Clone + Send + Sync,
-{
-    pub async fn new(client: &Client, sender: &AsyncSender<ResourceView>) -> Self {
-        let api: Api<T> = Api::all(client.clone());
-        let params = ListParams::default();
-
-        let (reader, writer) = reflector::store();
-        let rf = reflector(writer, watcher(api, params));
-        let sender = sender.clone();
-
-        tokio::spawn(async move {
-            let mut rfa = rf.applied_objects().boxed();
-            while let Ok(Some(event)) = rfa.try_next().await {
-                let _ = sender.send(Arc::new(event).into()).await;
-            }
-        });
-
-        Self { reader }
-    }
-}
-
 pub struct ReflectorRegistry {
     sender: AsyncSender<ResourceView>,
     client: Client,
-    reflectors: Vec<Box<dyn SpecViewAdapter>>,
+    readers: HashMap<GroupVersionKind, Box<dyn SpecViewAdapter>>,
 }
 
 impl ReflectorRegistry {
-    pub fn new(sender: AsyncSender<ResourceView>, client: Client) -> Self {
+    pub fn new(sender: AsyncSender<ResourceView>, client: &Client) -> Self {
         Self {
             sender,
-            client,
-            reflectors: Vec::default(),
+            client: client.clone(),
+            readers: HashMap::default(),
         }
     }
 
     pub async fn register<T>(&mut self)
     where
         T: Metadata<Ty = ObjectMeta>
-            // + Resource
             + 'static
             + Clone
             + Debug
             + Send
             + Sync
             + for<'de> k8s_openapi::serde::Deserialize<'de>,
+        ResourceView: From<Arc<T>>,
     {
-        // let a1: ResourceState<Pod> = ResourceState::new(&self.client, &self.sender).await;
         let api: Api<T> = Api::all(self.client.clone());
         let params = ListParams::default();
 
         let (reader, writer) = reflector::store();
         let rf = reflector(writer, watcher(api, params));
         let sender = self.sender.clone();
+
+        tokio::spawn(async move {
+            let mut rfa = rf.applied_objects().boxed();
+            while let Ok(Some(resource)) = rfa.try_next().await {
+                let _ = sender.send(Arc::new(resource).into()).await;
+            }
+        });
+
+        self.readers.insert(reader.gvk(), Box::new(reader));
     }
 }
 
-pub async fn bla() -> Result<Vec<Pod>> {
-    let client = Client::try_default().await?;
-    let (sender, receiver) = kanal::unbounded_async();
-
-    // let a: ResourceState<CustomResourceDefinition> = ResourceState::new(&client, &sender).await;
-    let a1: ResourceState<Pod> = ResourceState::new(&client, &sender).await;
-    let a2: ResourceState<Namespace> = ResourceState::new(&client, &sender).await;
-    let mut qwe: Vec<Arc<dyn SpecViewAdapter>> = vec![];
-    qwe.push(Arc::new(a1));
-    qwe.push(Arc::new(a2));
-
-    // let a: ResourceState<Pod> = ResourceState::new(&client, &sender).await;
-
-    tokio::time::sleep(Duration::from_secs(5)).await;
-    for q in qwe.iter() {
-        println!("{}, {:?}", q.kind(), q.items());
-    }
+pub async fn discover(client: &Client) -> Result<Vec<Pod>> {
+    let client = client.clone();
 
     let discovery = Discovery::new(client.clone()).run().await?;
+
     for group in discovery.groups() {
-        for (ar, caps) in group.recommended_resources() {
+        for (api_resource, caps) in group.recommended_resources() {
             if !caps.supports_operation(verbs::LIST) {
-                println!("!!! {:?}", ar);
                 continue;
             }
             let api: Api<DynamicObject> = if caps.scope == Scope::Cluster {
-                Api::all_with(client.clone(), &ar)
+                Api::all_with(client.clone(), &api_resource)
             } else {
-                Api::default_namespaced_with(client.clone(), &ar)
+                Api::default_namespaced_with(client.clone(), &api_resource)
             };
 
-            println!("{}/{} : {}", group.name(), ar.version, ar.kind);
+            println!(
+                "{}/{} : {}",
+                group.name(),
+                api_resource.version,
+                api_resource.kind
+            );
 
             let list = api.list(&Default::default()).await?;
             for item in list.items {
@@ -200,4 +165,22 @@ pub async fn bla() -> Result<Vec<Pod>> {
         }
     }
     Ok(vec![])
+}
+
+pub async fn discover_gvk(client: &Client) -> Result<Vec<GroupVersionKind>> {
+    let client = client.clone();
+
+    let discovery = Discovery::new(client.clone()).run().await?;
+
+    let mut result = vec![];
+    for group in discovery.groups() {
+        for (api_resource, caps) in group.recommended_resources() {
+            if !caps.supports_operation(verbs::LIST) {
+                continue;
+            }
+            let gvk = GroupVersionKind::gvk(group.name(), &api_resource.version, &api_resource.kind);
+            result.push(gvk);
+        }
+    }
+    Ok(result)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,45 @@
 use std::cmp::Ordering;
-use std::collections::BTreeMap;
-use std::sync::{Arc, LockResult, Mutex};
-use std::thread;
+use std::collections::{BTreeMap, HashMap};
 
+use anyhow::Result;
+use cruet::Inflector;
 use cursive::align::HAlign;
 use cursive::direction::Orientation;
-use cursive::traits::*;
-use cursive::view::scroll;
-use cursive::views::{Dialog, DummyView, LinearLayout, Menubar, Panel, ResizedView};
-use cursive::{event, menu, CursiveRunnable, Printer, Cursive};
-use cursive::reexports::crossbeam_channel::Sender;
 use cursive::reexports::log;
+use cursive::reexports::log::LevelFilter::{Debug, Info};
+use cursive::traits::*;
+use cursive::views::{Dialog, DummyView, LinearLayout, Menubar, Panel, ResizedView};
+use cursive::{event, menu, Cursive, CursiveRunnable, Printer};
 use cursive_table_view::{TableView, TableViewItem};
-use rand::{thread_rng, Rng};
-use crate::client::bla;
-use anyhow::Result;
-pub mod theme;
+use futures::StreamExt;
+use itertools::Itertools;
+use k8s_openapi::api::core::v1::{Namespace, Pod};
+use kube::api::GroupVersionKind;
+use kube::{Client, Resource, ResourceExt};
+
+use crate::client::{discover, discover_gvk, ReflectorRegistry, ResourceView};
+
 pub mod client;
+pub mod theme;
 
 #[derive(Copy, Clone, PartialEq, Eq, Hash)]
 enum PodColumn {
+    Namespace,
     Name,
-    Count,
-    Rate,
+    Ready,
+    Restarts,
+    Status,
+    Ip,
+    Node,
+    Age,
 }
 
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub struct PodView {
-    name: String,
-    count: usize,
-    rate: usize,
-}
-
-impl TableViewItem<PodColumn> for Arc<Mutex<PodView>> {
-    fn to_column(&self, column: PodColumn) -> String {
-        match self.lock() {
-            Ok(pod_guard) => pod_guard.to_column(column),
-            Err(err) => panic!("Error {}", err),
-        }
-    }
-
-    fn cmp(&self, other: &Self, column: PodColumn) -> Ordering
-    where
-        Self: Sized,
-    {
-        match (self.lock(), other.lock()) {
-            (Ok(self_pod_guard), Ok(other_pod_guard)) => {
-                self_pod_guard.cmp(&*other_pod_guard, column)
-            }
-            _ => panic!("Error"),
-        }
-    }
-}
-
-impl TableViewItem<PodColumn> for PodView {
+impl TableViewItem<PodColumn> for ResourceView {
     fn to_column(&self, column: PodColumn) -> String {
         match column {
-            PodColumn::Name => self.name.to_string(),
-            PodColumn::Count => format!("{}", self.count),
-            PodColumn::Rate => format!("{}", self.rate),
+            PodColumn::Namespace => self.namespace(),
+            PodColumn::Name => self.name(),
+            _ => String::new(),
         }
     }
 
@@ -67,34 +48,75 @@ impl TableViewItem<PodColumn> for PodView {
         Self: Sized,
     {
         match column {
-            PodColumn::Name => self.name.cmp(&other.name),
-            PodColumn::Count => self.count.cmp(&other.count),
-            PodColumn::Rate => self.rate.cmp(&other.rate),
+            PodColumn::Name => self.name().cmp(&other.name()),
+            PodColumn::Namespace => self.namespace().cmp(&other.namespace()),
+            _ => Ordering::Equal,
         }
     }
 }
 
-#[derive(Default)]
-struct GlobalState {
-    pods: BTreeMap<String, Arc<Mutex<PodView>>>,
+fn gvk_sort_key(gvk: &GroupVersionKind) -> (String, String, String) {
+    (gvk.group.clone(), gvk.version.clone(), gvk.kind.clone())
 }
 
+fn group_gvks(gvks: Vec<GroupVersionKind>) -> Vec<(String, Vec<GroupVersionKind>)> {
+    let mut misc = vec![];
+    let mut map = BTreeMap::new();
+
+    for gvk in gvks {
+        let grouper = if gvk.kind == "CustomResourceDefinition" {
+            "default"
+        } else if gvk.kind.starts_with("PersistentVolume") {
+            "storage"
+        } else if gvk.group.is_empty() {
+            "default"
+        } else if gvk.group.contains("admission") {
+            "admission"
+        } else if gvk.group.contains("flow") {
+            "flow"
+        } else {
+            gvk.group.split('.').next().unwrap()
+        }
+        .to_title_case();
+        map.entry(grouper).or_insert_with(Vec::new).push(gvk);
+    }
+
+    let default = map.remove("Default");
+    let mut grouped = vec![];
+    for (grouper, mut group) in map.into_iter() {
+        if group.len() == 1 {
+            misc.extend(group);
+        } else {
+            group.sort_unstable_by_key(gvk_sort_key);
+            grouped.push((grouper, group));
+        }
+    }
+    misc.sort_unstable_by_key(gvk_sort_key);
+
+    grouped.sort_unstable_by_key(|(name, _)| name.clone());
+    if let Some(group) = default {
+        grouped.insert(0, ("Default".to_string(), group));
+    }
+
+    grouped.push(("Misc".to_string(), misc));
+    grouped
+}
 
 fn main() -> Result<()> {
-    let rt = tokio::runtime::Builder::new_multi_thread()
-        .worker_threads(4)
+    let main_rt = tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(1)
         .enable_all()
-        .build()
-        .unwrap();
+        .build()?;
 
-    let a = rt.block_on(async move {
-        // sender.send(()).await.unwrap();
-        bla().await
-    })?;
+    let exchange_rt = tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(1)
+        .enable_all()
+        .build()?;
 
-    return Ok(());
+    let client = main_rt.block_on(async { Client::try_default().await })?;
 
-    let (sender, receiver) = kanal::unbounded_async::<()>();
+    let gvks = main_rt.block_on(async { discover_gvk(&client).await })?;
+    let grouped_gvks = group_gvks(gvks);
 
     let mut ui = CursiveRunnable::default();
     let mut ui = ui.runner();
@@ -102,14 +124,32 @@ fn main() -> Result<()> {
     ui.menubar()
         .add_subtree("File", menu::Tree::new().leaf("Exit", |s| s.quit()));
 
+    for (group_name, group) in grouped_gvks {
+        let mut group_tree = menu::Tree::new();
+        for gvk in group {
+            let leaf_name = if group_name == "Misc" {
+                format!("{}/{}/{}", gvk.group, gvk.version, gvk.kind)
+            } else {
+                format!("{}/{}", gvk.version, gvk.kind)
+            };
+            group_tree = group_tree.leaf(leaf_name, |s| s.quit());
+        }
+        ui.menubar().add_subtree(group_name, group_tree);
+    }
+
     ui.set_autohide_menu(false);
+
     cursive::logger::init();
+    log::set_max_level(Debug);
 
     let mut layout = LinearLayout::new(Orientation::Horizontal);
 
-    let table: TableView<PodView, PodColumn> = TableView::new().column(PodColumn::Name, "Name", |c| c.width_percent(20))
-        .column(PodColumn::Count, "Count", |c| c.align(HAlign::Center))
-        .column(PodColumn::Rate, "Rate", |c| {
+    let table: TableView<ResourceView, PodColumn> = TableView::new()
+        .column(PodColumn::Name, "Name", |c| c.width_percent(20))
+        .column(PodColumn::Namespace, "Namespace", |c| {
+            c.align(HAlign::Center)
+        })
+        .column(PodColumn::Name, "Rate", |c| {
             c.ordering(Ordering::Greater)
                 .align(HAlign::Right)
                 .width_percent(20)
@@ -117,25 +157,45 @@ fn main() -> Result<()> {
 
     layout.add_child(table.with_name("pods").full_screen());
 
-    // layout.add_child(Lol::default());
-
-    // layout.add_child(t1.min_size((32, 20)));
-    // layout.add_child(ResizedView::with_fixed_size((4, 0), DummyView));
-    // layout.add_child(create_table(container.clone()).min_size((32, 20)));
-
     let pods_panel = Panel::new(layout).title("Pods");
-    // ui.add_layer(pods_panel);
-
-    // ui.add_layer(Dialog::around(layout).title("Table View Demo"));
 
     ui.add_fullscreen_layer(pods_panel);
 
     ui.add_global_callback(event::Key::Esc, |s| s.select_menubar());
 
-    let sink: Sender<Box<dyn FnOnce(&mut Cursive) + Send>> = ui.cb_sink().clone();
+    let sink = ui.cb_sink().clone();
 
+    let (sender, receiver) = kanal::unbounded_async();
 
+    let registry = main_rt.block_on(async move {
+        let mut reg = ReflectorRegistry::new(sender, &client);
+        reg.register::<Pod>().await;
+        reg.register::<Namespace>().await;
+        reg
+    });
 
+    exchange_rt.spawn(async move {
+        let mut stream = receiver.stream();
+        while let Some(resource_view) = stream.next().await {
+            sink.send(Box::new(|siv| {
+                siv.call_on_name("pods", |table: &mut TableView<ResourceView, PodColumn>| {
+                    let q = resource_view.gvk();
+                    match resource_view {
+                        ResourceView::PodView(pod) => {
+                            let mut items = table.take_items();
+                            items.push(ResourceView::from(pod));
+                            table.set_items(items);
+                        }
+                        ResourceView::NamespaceView(_) => {
+                            println!("!");
+                        }
+                    }
+                });
+            }))
+            .unwrap();
+        }
+        panic!("!!")
+    });
 
     ui.add_global_callback(event::Key::F5, |s| {
         log::warn!("Or did it?");
@@ -146,32 +206,4 @@ fn main() -> Result<()> {
     ui.run();
 
     Ok(())
-}
-
-fn create_table(state: Arc<Mutex<GlobalState>>) -> TableView<Arc<Mutex<PodView>>, PodColumn> {
-    let pods = match state.lock() {
-        Ok(mut state) => {
-            state.pods.insert(
-                "a".to_string(),
-                Arc::new(Mutex::new(PodView {
-                    name: "a".to_string(),
-                    count: 0,
-                    rate: 0,
-                })),
-            );
-            state.pods.values().map(Arc::clone).collect::<Vec<_>>()
-        }
-        Err(err) => panic!("Error {}", err),
-    };
-
-    let a = TableView::<Arc<Mutex<PodView>>, PodColumn>::new()
-        .column(PodColumn::Name, "Name", |c| c.width_percent(20))
-        .column(PodColumn::Count, "Count", |c| c.align(HAlign::Center))
-        .column(PodColumn::Rate, "Rate", |c| {
-            c.ordering(Ordering::Greater)
-                .align(HAlign::Right)
-                .width_percent(20)
-        })
-        .items(pods);
-    a
 }
